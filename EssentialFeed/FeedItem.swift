6cc83912a672fd15174d8b4335f304278eebe067
Mushthak Ibrahim <mushthak.ibrahim@calpinetech.com>
2022-04-25 23:24:23 +0530

    Add the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mushthak Ibrahim on 4/25/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
