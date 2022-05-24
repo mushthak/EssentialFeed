d32ef61e00f727ddc84ee63152b18212f0f17944
Mushthak Ibrahim <mushthakebrahim@gmail.com>
2022-05-24 19:16:47 +0530

    Replace `reduce` function with latest `compactMapValues` function

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -115,9 +115,7 @@ class EssentialFeedTests: XCTestCase {
             "description" : item.description,
             "location" : item.location,
             "image" : item.imageURL.absoluteString
-        ].reduce(into: [String: Any]()) {(acc, e) in
-            if let value = e.value { acc[e.key] = value }
-        }
+        ].compactMapValues{ $0 }
 
         return (item, json)
     }
