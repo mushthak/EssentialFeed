402097c43f1b033a80829e4750f6bf1a53b6ff9e
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2022-11-07 22:03:43 +0530

    Move `FeedStore` to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Mushthak Ebrahim on 07/11/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timeStamp: Date, completion: @escaping InsertionCompletion)
+}
--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -36,10 +36,3 @@ public final class LocalFeedLoader {
         }
     }
 }
-
-public  protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timeStamp: Date, completion: @escaping InsertionCompletion)
-}
