b9d022395c8684b4e1f7d0341fc45d1093dc4f9d
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2022-11-12 20:33:15 +0530

    Move `LocalFeedLoader` and `RemoteFeedLoader` to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mushthak Ebrahim on 12/11/22.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
