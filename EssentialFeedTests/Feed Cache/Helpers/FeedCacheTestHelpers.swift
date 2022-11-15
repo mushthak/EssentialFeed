a0bcd0f62e24e3350bcc1dc7c5f0bd4d2eed19b3
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2022-11-15 20:05:50 +0530

    Separate Date extension helpers to distinct contexts

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -23,16 +23,17 @@ extension Date {
         return 7
     }
     
-    func minusFeedCacheMaxAge() -> Date {
-        return adding(days: -feedCacheMaxAgeInDays)
-    }
-    
-    func adding(days: Int) -> Date {
+    private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
     
+    func minusFeedCacheMaxAge() -> Date {
+        return adding(days: -feedCacheMaxAgeInDays)
+    }
+}
+
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
 }
-
