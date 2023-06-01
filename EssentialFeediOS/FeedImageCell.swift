810597644552bd14a9b7777e94b20764e7a79c1e
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2023-06-02 00:17:25 +0530

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Mushthak Ebrahim on 02/06/23.
+//
+
+import UIKit
+
+ public final class FeedImageCell: UITableViewCell {
+     public let locationContainer = UIView()
+     public let locationLabel = UILabel()
+     public let descriptionLabel = UILabel()
+ }
