0c7e4741c416510c47385fec623ddeae8a9a78c4
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2023-06-10 19:19:50 +0530

    Unify `FeedViewController` static factory helper into the `FeedUIComposer`

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -15,7 +15,7 @@ public final class FeedUIComposer {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
             MainQueueDispatchDecorator(decoratee: feedLoader))
         
-        let feedController = FeedViewController.makeWith(
+        let feedController = makeFeedViewController(
             delegate: presentationAdapter,
             title: FeedPresenter.title)
 
@@ -27,10 +27,8 @@ public final class FeedUIComposer {
         
         return feedController
     }
-}
 
-private extension FeedViewController {
-    static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
