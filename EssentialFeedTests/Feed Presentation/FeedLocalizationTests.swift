4a3b49103fe2e92f79bfb6a643f71d57d7f3d62e
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2023-06-23 05:53:20 +0530

    Move `FeedLocalizationTests` to cross-platform EssentialFeed module

--- EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
@@ -8,8 +8,6 @@
 import XCTest
 import EssentialFeed
 
-@testable import EssentialFeediOS
-
 final class FeedLocalizationTests: XCTestCase {
     
     func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
