9d3ad0a4ad6812dc0cafe2d407df2ce82f513bd6
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2023-06-23 05:36:26 +0530

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mushthak Ebrahim on 23/06/23.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
