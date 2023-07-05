45df9e79edb6bba372f95dd11d12ab4bfcc48306
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2023-07-05 21:04:25 +0530

    Move `FeedImageDataCache` to the `Feed Feature` production group

---
+++ EssentialFeed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageDataCache.swift
+//  EssentialFeed
+//
+//  Created by Mushthak Ebrahim on 05/07/23.
+//
+
+import Foundation
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
